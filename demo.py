b6bb701116f417d8528b719700548183ddf286fc
The Madras Techie <85541478+TheMadrasTechie@users.noreply.github.com>
2024-01-14 16:08:27 +0530

    Update demo.py
    Openai has disabled certain stuff .

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 import openai
 openai.api_key = "YOUR_API_KEY"
 
-model_engine = "text-davinci-003"
+#model_engine = "text-davinci-003"
+model_engine = "gpt-3.5-turbo-instruct"
 prompt = "நண்பா நண்பி எல்லாரும் வணக்கம் எப்படி இருக்கீங்க"
 completion = openai. Completion.create(engine=model_engine,prompt=prompt,max_tokens=1024, n=1,stop=None,temperature=0.5,)
 response = completion.choices[0].text
